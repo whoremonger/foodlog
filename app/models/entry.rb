ff1efd34a8cbbb6f39c40a478107f94b34ef9ce5
Jarrad L <incognito2500k@gmail.com>
2022-10-06 22:59:36 -0700

    Last commit

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,8 @@
 class Entry < ApplicationRecord
+  belongs_to :category
+  validates :meal_type, :calories, :proteins, :carbohydrates, :fats, :category_id, presence: true
+  
+  def day
+    self.created_at.strftime("%b %e, %Y")
+  end
 end
